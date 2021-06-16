0a55995ffe4a795a2eb4b3f070b4f043fad9f684
Thomas Greene <d4c213@gmail.com>
2021-06-16 16:52:52 +1000

    KnifeToMeetYou
    Basic Character model and knife script

---
+++ KnifeToMeetYou/Assets/Scripts/KnifeThrow.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeThrow : MonoBehaviour
+{
+    //Variables
+    public GameObject knife;
+    public Transform spawnPoint;
+    public float speed = 5f;
+
+    bool launched;
+
+
+    private void Start()
+    {
+
+    }
+
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+            launched = true;
+     
+    }
+
+    private void FixedUpdate()
+    {
+        if (launched)
+        {
+            ThrowKnife();
+        }
+    }
+
+
+    private void ThrowKnife()
+    {
+        GameObject knifeInstance = Instantiate(knife, spawnPoint.position, knife.transform.rotation);
+        knifeInstance.transform.rotation = Quaternion.LookRotation(-spawnPoint.forward);
+        Rigidbody knifeRig = knifeInstance.GetComponent<Rigidbody>();
+        
+
+
+        knifeRig.AddForce(spawnPoint.forward * speed, ForceMode.Impulse);
+        launched = false;
+    }
+}
